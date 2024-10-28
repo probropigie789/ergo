6014ab157ba3389985e81fd0255e0809c053e864
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-10-28 06:40:49 +0600

    updated index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch("/api/hello")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setData(data));
   }, []);
 
   return (
     <div>
       <h1>Home</h1>
       <p>Home page content</p>
+      {data && (
+        <div>
+          <h2>Data from API</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
