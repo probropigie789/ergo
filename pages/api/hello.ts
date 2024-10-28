0249ff77cafb36bdfe2e5be98aa54b2f5aa6de20
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-10-28 06:35:02 +0600

    added supabase client

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import supabase from "@/supabase/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
+  const { data, error } = await supabase.from("users").select("*");
+  console.log(data, error);
+
   res.status(200).json({ name: "John Doe" });
 }
